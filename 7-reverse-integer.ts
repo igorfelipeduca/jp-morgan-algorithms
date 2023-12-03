c28464ffc91af8df96aefd6c46f640e7e5d7d66b
Duca <yelldutz@gmail.com>
2023-12-03 00:36:32 -0300

    Create 7-reverse-integer.ts

---
+++ 7-reverse-integer.ts
@@ -0,0 +1,14 @@
+function reverse(x: number): number {
+    const string = JSON.stringify(x);
+    let parsedString = string.split("").reverse()
+
+    if(parsedString[parsedString.length - 1] === "-") {
+        parsedString.pop()
+        parsedString.splice(0, 0, "-")
+    }
+
+    if(Math.pow(2, 31) < Number(parsedString.join(""))) return 0
+    if(Math.pow(2, 31) * -1 > Number(parsedString.join(""))) return 0
+
+    return Number(parsedString.join(""))
+};
